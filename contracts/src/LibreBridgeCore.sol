d830b6b1472d6a784406068b087ca2ba76d81178
lbtns <lbtn@hrbr.site>
2024-07-12 17:20:26 +0800

    add domain and pass message

--- contracts/src/LibreBridgeCore.sol
+++ contracts/src/LibreBridgeCore.sol
@@ -19,6 +19,34 @@ contract LibreBridgeCore is Initializable, PausableUpgradeable, OwnableUpgradeab
         __UUPSUpgradeable_init();
     }
 
+    uint256 public thisChainId;
+
+    mapping(bytes32 => uint256) public domainNonces;
+
+    event PassMessage(
+        bytes32 indexed messageHash, uint256 toChain, address fromAppContract, address toAppContract, bytes message
+    );
+
+    function passMessage(uint256 toChain, address toAppContract, uint256 nonce, bytes calldata message) public {
+        bytes32 domain = computeDomain(thisChainId, toChain, msg.sender, toAppContract);
+
+        require(domainNonces[domain] == nonce, "Target nonce must be same");
+
+        bytes32 messageHash = keccak256(abi.encode(thisChainId, nonce, domain));
+
+        emit PassMessage(messageHash, toChain, msg.sender, toAppContract, message);
+
+        domainNonces[domain] += 1;
+    }
+
+    function computeDomain(uint256 fromChainId, uint256 toChainId, address fromAppContract, address toAppContract)
+        public
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encode(fromChainId, toChainId, fromAppContract, toAppContract));
+    }
+
     function pause() public onlyOwner {
         _pause();
     }
